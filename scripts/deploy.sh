cddf28bfb2c74571b04252ab1a1e313a5860193a
pedroarruda60663 <pg.arruda@campus.fct.unl.pt>
2025-03-02 18:06:04 +0000

    uptime-kuma with prometheus monitor added

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,28 +1,33 @@
 #!/bin/bash
 
+K8S_DIR="../.k8s"
+
 echo "Applying Kubernetes Namespace..."
-kubectl apply -f 00-namespace.yaml
+kubectl apply -f "$K8S_DIR/00-namespace.yaml"
 
 echo "Applying PVCs..."
-kubectl apply -f 90-pvc-heimdall.yaml
-kubectl apply -f 91-pvc-grafana.yaml
+kubectl apply -f "$K8S_DIR/90-pvc-heimdall.yaml"
+kubectl apply -f "$K8S_DIR/91-pvc-grafana.yaml"
+kubectl apply -f "$K8S_DIR/95-pvc-uptimekuma.yaml"
 
 echo "Applying ConfigMaps..."
-kubectl apply -f 80-config-heimdall.yaml
-kubectl apply -f 83-config-prometheus.yaml
+kubectl apply -f "$K8S_DIR/80-config-heimdall.yaml"
+kubectl apply -f "$K8S_DIR/83-config-prometheus.yaml"
 
 echo "Deploying Applications..."
-kubectl apply -f 20-deployment-heimdall.yaml
-kubectl apply -f 21-deployment-grafana.yaml
-kubectl apply -f 22-deployment-glances.yaml
-kubectl apply -f 23-deployment-prometheus.yaml
-kubectl apply -f 24-deployment-node.yaml
+kubectl apply -f "$K8S_DIR/20-deployment-heimdall.yaml"
+kubectl apply -f "$K8S_DIR/21-deployment-grafana.yaml"
+kubectl apply -f "$K8S_DIR/22-deployment-glances.yaml"
+kubectl apply -f "$K8S_DIR/23-deployment-prometheus.yaml"
+kubectl apply -f "$K8S_DIR/24-deployment-node.yaml"
+kubectl apply -f "$K8S_DIR/25-deployment-uptimekuma.yaml"
 
 echo "Exposing Services..."
-kubectl apply -f 30-service-heimdall.yaml
-kubectl apply -f 31-service-grafana.yaml
-kubectl apply -f 32-service-glances.yaml
-kubectl apply -f 33-service-prometheus.yaml
-kubectl apply -f 34-service-node.yaml
+kubectl apply -f "$K8S_DIR/30-service-heimdall.yaml"
+kubectl apply -f "$K8S_DIR/31-service-grafana.yaml"
+kubectl apply -f "$K8S_DIR/32-service-glances.yaml"
+kubectl apply -f "$K8S_DIR/33-service-prometheus.yaml"
+kubectl apply -f "$K8S_DIR/34-service-node.yaml"
+kubectl apply -f "$K8S_DIR/35-service-uptimekuma.yaml"
 
 echo "Completed."
